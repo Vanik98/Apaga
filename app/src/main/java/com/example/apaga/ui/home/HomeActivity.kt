c285961081a561429a80e16df0b2c2770766c774
Vanik98 <42978851+Vanik98@users.noreply.github.com>
2020-02-16 14:49:57 +0400

    update

--- app/src/main/java/com/example/apaga/ui/home/HomeActivity.kt
+++ app/src/main/java/com/example/apaga/ui/home/HomeActivity.kt
@@ -1,6 +1,5 @@
 package com.example.apaga.ui.home
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.apaga.R
 import com.example.apaga.di.component.AppComponent
