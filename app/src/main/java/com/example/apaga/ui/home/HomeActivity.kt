3531e8e046e277c94e30bf6211fe188a85a56330
Vanik Dallakyan <vanik.dallakyan@betconstruct.com>
2020-02-19 15:10:35 +0400

    update

--- app/src/main/java/com/example/apaga/ui/home/HomeActivity.kt
+++ app/src/main/java/com/example/apaga/ui/home/HomeActivity.kt
@@ -49,7 +49,7 @@ class HomeActivity : BaseActivity(),HomeContract.View {
         val navController = findNavController(R.id.nav_host_fragment)
 
         appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
+                R.id.nav_payment, R.id.nav_buy_bags, R.id.nav_slideshow,
                 R.id.nav_tools, R.id.nav_share, R.id.nav_send), drawerLayout)
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
