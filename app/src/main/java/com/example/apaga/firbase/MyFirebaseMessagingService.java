d3c36e78127ebe16d3b8076e1c38d6921f7e8cc9
Vanik98 <42978851+Vanik98@users.noreply.github.com>
2020-06-09 22:01:10 +0400

    fix part1

--- app/src/main/java/com/example/apaga/firbase/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/apaga/firbase/MyFirebaseMessagingService.java
@@ -1,63 +1,63 @@
-package com.example.apaga.firbase;
-
-import android.app.NotificationManager;
-import android.content.Context;
-import android.content.Intent;
-import android.media.RingtoneManager;
-import android.net.Uri;
-
-import com.example.apaga.R;
-import com.google.firebase.messaging.FirebaseMessagingService;
-import com.google.firebase.messaging.RemoteMessage;
-
-import androidx.core.app.NotificationCompat;
-
-public class MyFirebaseMessagingService extends FirebaseMessagingService {
-
-    private Intent mIntent;
-
-
-    private MyFirebaseMessagingService(Intent mIntent) {
-        this.mIntent = mIntent;
-    }
-
-
-    public static MyFirebaseMessagingService getMyFirebaseMessagingService(Intent intent) {
-
-        //TODO check null for singltone
-        return new MyFirebaseMessagingService(intent);
-    }
-
-
-    public MyFirebaseMessagingService() {
-    }
-
-
-    @Override
-    public void onMessageReceived(RemoteMessage remoteMessage) {
-        sendNotification(remoteMessage.getNotification().getBody());
-
-    }
-
-    public void sendNotification(String messageBody) {
-
-//        mIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-
-        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-
-        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
-                .setPriority(NotificationCompat.PRIORITY_HIGH)
-                .setDefaults(NotificationCompat.DEFAULT_ALL)
-                .setSmallIcon(R.drawable.ic_launcher_foreground)
-                .setContentTitle(this.getString(R.string.app_name))
-                .setContentText(messageBody)
-                .setAutoCancel(true)
-                .setSound(defaultSoundUri);
-
-        NotificationManager notificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-
-        notificationManager.notify(0, notificationBuilder.build());
-    }
-
-}
+//package com.example.apaga.firbase;
+//
+//import android.app.NotificationManager;
+//import android.content.Context;
+//import android.content.Intent;
+//import android.media.RingtoneManager;
+//import android.net.Uri;
+//
+//import com.example.apaga.R;
+//import com.google.firebase.messaging.FirebaseMessagingService;
+//import com.google.firebase.messaging.RemoteMessage;
+//
+//import androidx.core.app.NotificationCompat;
+//
+//public class MyFirebaseMessagingService extends FirebaseMessagingService {
+//
+//    private Intent mIntent;
+//
+//
+//    private MyFirebaseMessagingService(Intent mIntent) {
+//        this.mIntent = mIntent;
+//    }
+//
+//
+//    public static MyFirebaseMessagingService getMyFirebaseMessagingService(Intent intent) {
+//
+//        //TODO check null for singltone
+//        return new MyFirebaseMessagingService(intent);
+//    }
+//
+//
+//    public MyFirebaseMessagingService() {
+//    }
+//
+//
+//    @Override
+//    public void onMessageReceived(RemoteMessage remoteMessage) {
+//        sendNotification(remoteMessage.getNotification().getBody());
+//
+//    }
+//
+//    public void sendNotification(String messageBody) {
+//
+////        mIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//
+//        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+//
+//        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+//                .setPriority(NotificationCompat.PRIORITY_HIGH)
+//                .setDefaults(NotificationCompat.DEFAULT_ALL)
+//                .setSmallIcon(R.drawable.ic_launcher_foreground)
+//                .setContentTitle(this.getString(R.string.app_name))
+//                .setContentText(messageBody)
+//                .setAutoCancel(true)
+//                .setSound(defaultSoundUri);
+//
+//        NotificationManager notificationManager =
+//                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+//
+//        notificationManager.notify(0, notificationBuilder.build());
+//    }
+//
+//}
